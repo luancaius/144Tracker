d9563464bdd64e9886e1c71a44f843bb2776615e
luancaius <luancaius@outlook.com>
2019-10-22 21:08:03 -0400

    Initial background project

---
+++ dotnetcore/src/GrabData/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace GrabData
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+        .ConfigureServices(services =>
+        {
+            services.AddHostedService<RawService>();
+        });
+    }
+}
---
+++ dotnetcore/src/GrabData/RawService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrabData
+{
+    public class RawService : IHostedService
+    {
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(HelloWorld, null, 0, 5000);
+            Console.WriteLine("GrabData - Start");
+
+            return Task.CompletedTask;
+        }
+
+        void HelloWorld(object state)
+        {
+            Console.WriteLine($"Time - {DateTime.Now.ToLongTimeString()}");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            //New Timer does not have a stop. 
+            _timer?.Change(Timeout.Infinite, 0);
+            Console.WriteLine("GrabData - Stop");
+            return Task.CompletedTask;
+        }
+    }
+}
