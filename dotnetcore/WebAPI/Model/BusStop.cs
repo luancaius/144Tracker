cb3db17a8435d1b63e2b0a7d30ac73250ae2c812
luancaius <luancaius@outlook.com>
2019-12-29 23:53:30 -0500

    feat: adding busstop controller

---
+++ dotnetcore/WebAPI/Model/BusStop.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Model
+{
+    public class BusStop
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Distance { get; set; }
+    }
+}
