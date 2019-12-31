b9b3199a1b7299419af00af82e338a90e70bb53b
luancaius <luancaius@outlook.com>
2019-12-30 23:46:32 -0500

    fix: fixing startup for the route

--- dotnetcore/WebAPI/Startup.cs
+++ dotnetcore/WebAPI/Startup.cs
@@ -26,6 +26,8 @@ namespace WebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
+
+            services.AddMvc(a => { a.EnableEndpointRouting = false;});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,17 +47,8 @@ namespace WebAPI
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-            
-            app.UseHttpsRedirection();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseMvc();
         }
     }
 }
